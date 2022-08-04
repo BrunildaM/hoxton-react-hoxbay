49f862c72470bf87131324dee3b74a4e44da30dd
Brunilda Muco <brunildamuco6@gmail.com>
2022-08-05 00:25:08 +0200

    navigate to the basket

--- src/Pages/Basket.tsx
+++ src/Pages/Basket.tsx
@@ -1,37 +1,88 @@
+import { useEffect, useState } from "react"
+
+type ItemInBasket = {
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    categoryId: number,
+    image: string,
+    quantity: number
+}
+
+
+function getTotalPrice (basket: ItemInBasket[]) {
+    let total = 0
+    for(const item of basket) {
+        total += item.quantity * item.price
+    }
+    
+    return total.toFixed(2)
+}
+
 function Basket() {
+    const [basket, setBasket] = useState<ItemInBasket[]>([])
+
+    useEffect(() => {
+        fetch('http://localhost:4000/basket')
+            .then(resp => resp.json())
+            .then(basketToDIsplay => setBasket(basketToDIsplay))
+    }, [])
+
+
+    function changeQuantity (item: ItemInBasket, updatedQuantity: number) {
+        let basketCopy = JSON.parse(JSON.stringify(basket))
+
+        if(updatedQuantity > 0 ){
+            let match = basketCopy.find((target: ItemInBasket) => target.id === item.id)
+        match.quantity = updatedQuantity
+        } else {
+            basketCopy= basketCopy.filter((target: ItemInBasket)  => target.id !== item.id)
+
+        }
+        setBasket(basketCopy)
+    }
+
+
     return (
         <section className="basket-container">
             <h2>Your Basket</h2>
             <ul>
-                {/* <!-- Basket Item --> */}
-                <li>
-                    <article className="basket-container__item">
-                        <img
-                            src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-                            alt="Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
-                            width="90"
-                        />
-                        <p>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</p>
-                        <p>
-                            Qty:
-                            <select>
-                                <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            </select>
-                            
-                        </p>
-                        {/* <!-- The item total is calculated using the Qty selected value --> */}
-                        <p>Item total: £109.95</p>
-                    </article>
-                </li>
-                {/* <!--  --> */}
+                {basket.map(itemInBasket =>
+                    <li key={itemInBasket.id}>
+                        <article className="basket-container__item">
+                            <img
+                                src={itemInBasket.image}
+                                alt={itemInBasket.title}
+                                width="90"
+                            />
+                            <p>{itemInBasket.title}</p>
+                            <p>
+                                Qty:
+                                <select 
+                                defaultValue={itemInBasket.quantity}
+                                onChange={e => {
+                                    changeQuantity(itemInBasket, Number(e.target.value))
+                                }}
+                                >
+                                    <option value="0">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                </select>
+
+                            </p>
+                           
+                            <p>Item total: £{(itemInBasket.price * itemInBasket.quantity).toFixed(2)}</p>
+                        </article>
+                    </li>
+                    )}
             </ul>
             {/* <!-- Basket total is calculated using each item's total from above --> */}
-            <h3>Your total: £109.95</h3>
+            <h3>Your total: £{getTotalPrice(basket)}</h3>
         </section>
     )
 }
 
 export default Basket
+
--- src/Pages/ProductDetails.tsx
+++ src/Pages/ProductDetails.tsx
@@ -1,25 +1,45 @@
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
-type Product ={
+type Product = {
     id: number,
-      title: string,
-      price: number,
-      description: string,
-      categoryId: number,
-      image: string
-  }
+    title: string,
+    price: number,
+    description: string,
+    categoryId: number,
+    image: string
+}
+
+
 
 function ProductDetails() {
-    
-    const [product, SetProduct] = useState(null)
+    {/* @ts-ignore */ }
+    const [product, SetProduct] = useState<Product>({})
     const params = useParams()
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch(`http://localhost:4000/products/${params.id}`)
-        .then(resp => resp.json())
-        .then(productToDisplay => SetProduct(productToDisplay))
+            .then(resp => resp.json())
+            .then(productToDisplay => SetProduct(productToDisplay))
     }, [])
+
+
+    function addProductToBasket() {
+        fetch('http://localhost:4000/basket', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ ...product, quantity: 1 })
+        })
+            .then(resp => resp.json())
+            .then(() => {
+                navigate('/basket')
+            })
+
+    }
+
 
 
     if ((product) === null) return <p>Select one product</p>
@@ -37,8 +57,8 @@ function ProductDetails() {
                     {product.description}
                 </p>
                 <p>£{product.price}</p>
-                {/* <!-- Once you click in this button, the user should be redirected to the Basket page --> */}
-                <button>Add to basket</button>
+
+                <button onClick={() => addProductToBasket()}>Add to basket</button>
             </div>
         </section>
     )
